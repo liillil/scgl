ed66ec7dee998f8a8565efa0cf0bf3f07612ce4e
ligang <ligang@365haoyou.com>
2024-08-02 21:01:37 +0800

    update

---
+++ src/main/java/com/example/scgl/ScglApplication.java
@@ -0,0 +1,13 @@
+package com.example.scgl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScglApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScglApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/scgl/ScglApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scgl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScglApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
