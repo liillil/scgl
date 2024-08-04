f29ca2b8153ff9cd717548cf6d4ee335b09362e8
ligang <ligang@365haoyou.com>
2024-08-04 16:47:55 +0800

    比价

--- src/main/java/com/naver/goods/ScglApplication.java
+++ src/main/java/com/naver/goods/ScglApplication.java
@@ -1,11 +1,13 @@
 package com.naver.goods;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@MapperScan("com.naver.goods.mapper")
 public class ScglApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/naver/goods/entity/ShopInfoTb.java
@@ -0,0 +1,15 @@
+package com.naver.goods.entity;
+
+import lombok.Data;
+
+/**
+ * @ClassName: ShopInfoTb
+ * @Description:
+ * @Author: lg
+ * @Date: 2024/8/4 16:42
+ */
+@Data
+public class ShopInfoTb {
+    private Long id;
+    private String shopName;
+}
